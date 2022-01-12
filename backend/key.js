e26456c8fd36a5b0a8b3e8bda4abc1491e3305c5
Yeferal <yefercuber20@gmail.com>
2022-01-11 22:05:53 -0600

    actualizacion

--- backend/key.js
+++ backend/key.js
@@ -2,11 +2,11 @@ module.exports = {
 
     database: {
         connectionLimit: 10,
-        user: "userComercio",
-        password: "1234",
-        database: "fruitshop",
-        host: "localhost",
-        // port: 3306
+        user: "uylwpnqfdbrqqvrt",
+        password: "fwgSHPgLB0qfGoKcAJ6E",
+        database: "brebia6szdrtwav9fna1",
+        host: "brebia6szdrtwav9fna1-mysql.services.clever-cloud.com",
+        port: 3306
     }
 
 };
