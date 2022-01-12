cc29a2982a96cc67310d911b73ab42c8be0d9c2d
Yeferal <yefercuber20@gmail.com>
2022-01-12 00:07:38 -0600

    actualizacion

--- frontend/src/app/services/global.ts
+++ frontend/src/app/services/global.ts
@@ -1,6 +1,6 @@
 export var GLOBAL = {
-    URL: "https://accomerce-app.herokuapp.com/api",
+    URL: "https://ss1-eccomerce.herokuapp.com/api",
     // URL: "http://localhost:65000/api",
-    URL2: "https://accomerce-app.herokuapp.com"
+    URL2: "https://ss1-eccomerce.herokuapp.com"
     // URL2: "http://localhost:65000"
 }
