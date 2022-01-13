9581d4a0e0f992a99fe1977eef3e276485a92900
Yeferal <yefercuber20@gmail.com>
2022-01-13 17:01:28 -0600

    actualizacion server

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
-    // database: {
-    //         username: "uylwpnqfdbrqqvrt",
-    //         password: "fwgSHPgLB0qfGoKcAJ6E",
-    //         database: "brebia6szdrtwav9fna1",
-    //         host: "brebia6szdrtwav9fna1-mysql.services.clever-cloud.com",
-    //         port: 3306  
-    // },
     database: {
-        username: "root",
-        password: "1234",
-        database: "fruitshop",
-        host: "localhost",
-        user: "root",
-        // port: 3306  
-    }
+            username: "uylwpnqfdbrqqvrt",
+            password: "fwgSHPgLB0qfGoKcAJ6E",
+            database: "brebia6szdrtwav9fna1",
+            host: "brebia6szdrtwav9fna1-mysql.services.clever-cloud.com",
+            port: 3306  
+    },
+    // database: {
+    //     username: "root",
+    //     password: "1234",
+    //     database: "fruitshop",
+    //     host: "localhost",
+    //     user: "root",
+    //     // port: 3306  
+    // }
 }
